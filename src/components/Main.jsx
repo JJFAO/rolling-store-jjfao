88d5b6168b61261d2a3b6d91cf498059d941a5b9
Juan <jjfalonso@gmail.com>
2020-05-09 17:14:24 -0300

    install antd, create Main and Results

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div>
+                <img src="/rollingstore.png" className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        )
+    }
+}
