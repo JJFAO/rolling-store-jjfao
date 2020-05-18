fe965bbcd22bd59b220acbd988f88d4bbc2bd54f
JJFAO <jjfalonso@gmail.com>
2020-05-18 06:00:23 -0300

    images added to ProductCard and styling on images to fit content

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,10 +3,17 @@ import { Link } from 'react-router-dom'
 import { Button } from 'antd'
 
 export default class ProductCard extends Component {
+
+    getUrl = (id) => (`https://firebasestorage.googleapis.com/v0/b/rolling-store-jjfao.appspot.com/o/${id}.png?alt=media`)
+
     render() {
         const { name, brand, price, id } = this.props.product
         return (
             <div className="product-card">
+                <div style={{height: '400px', overflow: 'hidden', display: 'flex', alignItems: 'center', background: 'white' }}>
+                    <img src={this.getUrl(id)} alt=""
+                    style={{width: '100%'}} />
+                </div>
                 <div>producto: {name}</div>
                 <div>marca: {brand}</div>
                 <div>precio: {price}</div>
