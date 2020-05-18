71929d879478ab880e57779a5361a13ba158e86c
JJFAO <jjfalonso@gmail.com>
2020-05-18 05:43:21 -0300

    Added Button and Link to product/:id in ProductCard; firebase connection to get product by id in Product; render product details and its image

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom';
+import { firebaseApp } from "../utils/firebase";
 
-export default class Product extends Component {
+class Product extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            product: {}
+        }
+    }
+
+    getUrl = (id) => (`https://firebasestorage.googleapis.com/v0/b/rolling-store-jjfao.appspot.com/o/${id}.png?alt=media`)
+
+    componentDidMount() {
+        const ref = firebaseApp.database().ref().child('products');
+        const { id } = this.props.match.params;
+
+        ref.orderByChild("id").equalTo(id).on("value", async (snapshot) => {
+            console.log(snapshot.toJSON()[id]);
+
+            this.setState({ product: snapshot.toJSON()[id] });
+        });
+
+    }
+
     render() {
+        const { name, price, brand, id } = this.state.product;
         return (
             <div>
-                Página del producto
+                <h3> {name} </h3>
+                <img src={this.getUrl(id)} alt="" style={{width: '100%', maxWidth: '400px'}} />
+                <p>Marca: {brand} </p>
+                <p>Precio: {price} </p>
             </div>
         )
     }
 }
+
+export default withRouter(Product);
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'antd'
 
 export default class ProductCard extends Component {
     render() {
-        const { name, brand, price } = this.props.product
+        const { name, brand, price, id } = this.props.product
         return (
             <div className="product-card">
                 <div>producto: {name}</div>
                 <div>marca: {brand}</div>
                 <div>precio: {price}</div>
+                <Button type="primary">
+                    <Link to={`/product/${id}`}>
+                        Ver más
+                    </Link>
+                </Button>
             </div>
         )
     }
